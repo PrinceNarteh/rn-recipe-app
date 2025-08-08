421a9652bc5d5dda9a0edebd7998b895f9f57b07
Prince Narteh <prinarteh@gmail.com>
2025-08-08 21:26:12 +0000

    feat: minor changes

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,11 @@
+import { Text, View, StyleSheet, Image } from "react-native";
+
+const _layout = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default _layout;
